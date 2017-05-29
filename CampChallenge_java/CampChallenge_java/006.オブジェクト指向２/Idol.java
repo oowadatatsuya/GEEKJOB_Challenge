5749dc374a09c450f7f55debf6abc6243a74b5a5
oowadatatsuya <hinatanonaegi@gmail.com>
2017-05-29 15:14:59 +0900

    クラス課題　クラスの継承　提出します

---
+++ CampChallenge_java/CampChallenge_java/006.オブジェクト指向２/Idol.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kusasukadai.sample;
+
+import java.io.PrintWriter;
+
+
+
+/**
+ *
+ * @author kumi
+ */
+public class Idol {
+     //フィールド
+    public String name = "" ;
+    public int age= 0 ;
+    //メソッド
+    public void setIdol(String a,int n){
+        
+        //　引数をフィールドへ
+        this.name = a;
+        this.age = n;
+    
+    }
+    
+public void output (PrintWriter out){
+       out.print("名前は" + this.name + "。<br>");
+       out.print("年齢は" + this.age + "歳。");
+ }
+
+    /**
+     *
+     * @param args
+     */
+    public void teigi(String[] args){
+   // クラスにインスタンス生成
+      Idol uzuki = new Idol();
+   // インスタンスのメソッド利用 
+    uzuki.setIdol("島村凛",15);
+  }
+    
+    
+}
---
+++ CampChallenge_java/CampChallenge_java/006.オブジェクト指向２/utikeshi.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kusasukadai.sample;
+
+
+/**
+ *
+ * @author kumi
+ */
+public class utikeshi extends Idol{
+
+    /**
+     *
+     */
+    public void setIdol(){
+        this.name = "";
+        this.age = 0;
+    
+    }
+
+}
