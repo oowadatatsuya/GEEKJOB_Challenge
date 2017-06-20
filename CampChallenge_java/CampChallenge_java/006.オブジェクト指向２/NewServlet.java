404d5a23dee0a48619ef489470a686a434db9294
oowadatatsuya <hinatanonaegi@gmail.com>
2017-06-20 16:48:54 +0900

    DB操作課題　全情報の取得と表示　提出します。

---
+++ CampChallenge_java/CampChallenge_java/006.オブジェクト指向２/NewServlet.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kumi
+ */
+public class NewServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+            Connection db_con = null;
+    PreparedStatement db_st = null;
+    ResultSet db_data = null;
+       try
+{ 
+
+    Class.forName("com.mysql.jdbc.Driver").newInstance();
+    db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GEEKJOB_db","oowada","gomibako");
+    System.out.print("接続を確認");
+//    
+//    db_st = db_con.prepareStatement("insert into proflies values (?,?,?,?,?)");
+//    db_st.setInt(1, 6);
+//    db_st.setString(2, "城嶋 茂");
+//    db_st.setString(3, "000-1111-2222");
+//    db_st.setInt(4, 46);
+//    db_st.setString(5, "19701117");
+//    
+//    db_st.executeUpdate();
+//    System.out.print("情報の追加を確認");
+    
+    db_st = db_con.prepareStatement("select * from proflies");
+    
+    
+    db_data = db_st.executeQuery();
+    while(db_data.next()){
+        System.out.print("ID:" + db_data.getInt("profilesID") + "<br>");
+        System.out.print("名前:" + db_data.getString("name") + "<br>");
+        System.out.print("番号:" + db_data.getString("tell") + "<br>");
+        System.out.print("年齢:" + db_data.getInt("age") + "<br>");
+        System.out.print("誕生日:" + db_data.getString("birthday") + "<br>");
+    }
+    
+    System.out.print("情報の表示完了");
+ 
+    db_data.close();
+    db_st.close();
+    db_con.close();
+
+        }catch(SQLException e_sql){
+            System.out.print("1:接続時にエラーが発生しました:" + e_sql.toString());
+        }catch(Exception e){
+            System.out.print("2:接続時にエラーが発生しました：" + e.toString());
+      
+        }finally{
+            if(db_con != null){
+                try{
+                 db_con.close();
+        }catch(Exception e_con){
+            System.out.println(e_con.getMessage());
+        }       
+    }
+}
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
